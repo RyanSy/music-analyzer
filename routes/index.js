01256bb7bcded2dfd57b255d4d8fe903f8096de3
Ryan Sy <swerve@MacBook-Pro.fios-router.home>
2017-02-13 16:08:59 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,33 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var app = express();
 var request = require('request');
-var client_id = '071dc603c44247319f007ce6bc89be08';
-var client_secret = 'b769223019aa445f991143cc713a0297';
+var client_id = process.env.CLIENT_ID;
+var client_secret = process.env.CLIENT_SECRET;
 
+// will refactor using SpotifyWebApi for API requests
 var SpotifyWebApi = require('spotify-web-api-node');
 var spotifyApi = new SpotifyWebApi({
-  clientId : '071dc603c44247319f007ce6bc89be08',
-  clientSecret : 'b769223019aa445f991143cc713a0297',
+  clientId : client_id,
+  clientSecret : client_secret,
   redirectUri : 'http://localhost:3000/results'
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/* GET home page. */
+// Render home page
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Music Analyzer', author: 'Ryan Sy'});
+  res.render('index', {title: 'Music Analyzer'});
 });
 
 // Analyze Track
-router.post('/search', function(req, res, next) {
+router.post('/audiofeatures', function(req, res, next) {
   // console.log('req.body =>', req.body);
-
-  var authOptions = {
+  var trackTitle = req.body.trackName;
+  var trackTitleModified = trackTitle.replace(/ /g, '+');
+  console.log('trackTitleModified => ', trackTitleModified);
+  // get access token
+  var getToken = {
     url: 'https://accounts.spotify.com/api/token',
     headers: {
       'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
@@ -35,38 +39,83 @@ router.post('/search', function(req, res, next) {
     },
     json: true
   };
-
-  request.post(authOptions, function(error, response, body) {
+  request.post(getToken, function(error, response, body) {
     if (!error && response.statusCode === 200) {
+      // console.log('* POST request for access token response body => ', body);
       var access_token = body.access_token;
     } else {
       console.log(error);
     }
 
     // get track info request
-    request('https://api.spotify.com/v1/search?q=' + req.body.trackName + '&type=track', function(error, response, body) {
+    request('https://api.spotify.com/v1/search?q=' + trackTitleModified + '&type=artist,track', function(error, response, body) {
       if (!error && response.statusCode == 200) {
+        // console.log('* GET request for track info response body => ', body);
         var parsedBody = JSON.parse(body);
         var trackInfo = parsedBody.tracks.items[0];
+        // console.log('trackInfo => ', trackInfo);
         var trackId = trackInfo.id;
-        console.log('trackInfo =>', trackInfo);
       } else {
         console.log(error);
       }
-      // use the access token to access the Spotify Web API
-      var options = {
+
+      // use the access token to access the audio features endpoint
+      var audioFeatures = {
         url: 'https://api.spotify.com/v1/audio-features/' + trackId,
         headers: {
           'Authorization': 'Bearer ' + access_token
         },
         json: true
       };
-      request.get(options, function(error, response, body) {
-        console.log(body);
-        res.json(body);
-      });
-    });
+      request.get(audioFeatures, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          // console.log('* GET request for track audio features response body => ', body);
+          var hbsObject = {
+            access_token: access_token,
+            artist: trackInfo.artists[0].name,
+            title: trackInfo.name,
+            id: trackInfo.id,
+            danceability: body.danceability,
+            energy: body.energy,
+            key: body.key,
+            loudness: body.loudness,
+            mode: body.mode,
+            speechiness: body.speechiness,
+            acousticness: body.acousticness,
+            instrumentalness: body.instrumentalness,
+            liveness: body.liveness,
+            valence: body.valence,
+            tempo: body.tempo,
+            duration_ms: body.duration_ms,
+            time_signature: body.time_signature
+          }
+          res.render('audiofeatures', hbsObject);
+        } else {
+          console.log(error);
+        }
+
+      });// end get request for track audio features
+    });// end get track info request
   }); // end post request for access token
-}); // end post route
+}); // end audiofeatures post route
+
+router.post('/audioanalysis', function(req, res, next) {
+    // use the access token to access the audio features endpoint
+    var audioAnalysis = {
+      url: 'https://api.spotify.com/v1/audio-analysis/' + req.body.id,
+      headers: {
+        'Authorization': 'Bearer ' + req.body.access_token
+      },
+      json: true
+    };
+    request.get(audioAnalysis, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        // console.log('* GET request for track audio analysis response body => ', body);
+        res.json(body);
+      } else {
+        console.log(error);
+      }
+    });// end get request for track audio features
+}); // end audioanalysis post route
 
 module.exports = router;
